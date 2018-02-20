ac78b93208921ce5c5246badd3cacd26226bf5d9
kpaszkowski <krzysztof.paszkowski@pentacomp.pl>
2018-02-20 22:10:38 +0100

    crud

---
+++ SpiceManager/SpiceManager/Messages/ValidationMessages.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpiceManager.Messages
+{
+    public static class ValidationMessages
+    {
+        public const string NieWybranoElementuDoUsuniecia = "Nie wybrano elementu do usunięcia.";
+        public const string NieWybranoElementuDoEdycji = "Nie wybrano elementu do edycji.";
+        public const string ZleParametry = "Podano niewłaśniwe dane.";
+        public const string NazwaPrzyprawyNieJestUnikalna = "Istnieje już taka przyprawa.";
+        public const string NazwaProduktuNieJestUnikalna = "Istnieje już taki produkt.";
+        public const string BrakPrzyprawy = "Brakuje {0} gram przyprawy \"{1}\".\nNajpierw dodaj wymaganą ilość do magazynu.";
+        public const string NiePodanoNazwy= "Nie podano nazwy.";
+    }
+}
--- SpiceManager/SpiceManager/ViewModel/ViewModelMain.cs
+++ SpiceManager/SpiceManager/ViewModel/ViewModelMain.cs
@@ -1,9 +1,15 @@
-﻿using System;
+﻿using SpiceManager.Messages;
+using SpiceManager.Other;
+using SpiceManager.WindowView.ProductWindow;
+using SpiceManager.WindowView.SpiceWindow;
+using SpiceManager.WindowView.WarehouseWindow;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace SpiceManager
 {
@@ -21,21 +27,26 @@ namespace SpiceManager
         #region Command
 
         public RelayCommand AddNewProductCommand { get; set; }
+        public RelayCommand AddProductToBaseCommand { get; set; }
         public RelayCommand RemoveProductCommand { get; set; }
         public RelayCommand EditProductCommand { get; set; }
 
         public RelayCommand AddNewSpiceCommand { get; set; }
+        public RelayCommand AddSpiceToBaseCommand { get; set; }
         public RelayCommand RemoveSpiceCommand { get; set; }
         public RelayCommand EditSpiceCommand { get; set; }
+        public RelayCommand EditSpiceInBaseCommand { get; set; }
 
         public RelayCommand AddSpiceToWarehouseCommand { get; set; }
+        public RelayCommand AddSpiceToWarehouseInBaseCommand { get; set; }
         public RelayCommand RemoveSpiceFromWarehouseCommand { get; set; }
         public RelayCommand EditSpiceInWarehouseCommand { get; set; }
 
         public RelayCommand StartProductionCommand { get; set; }
         public RelayCommand ExportToExcelCommand { get; set; }
         public RelayCommand SaveCommand { get; set; }
-
+        public RelayCommand CloseWindowCommand { get; set; }
+        public RelayCommand ShowInfoCommand { get; set; }
         #endregion
 
         #region Selected Items
@@ -148,20 +159,27 @@ namespace SpiceManager
 
 
             AddNewProductCommand = new RelayCommand(AddNewProduct);
+            AddProductToBaseCommand = new RelayCommand(AddProductToBase);
             RemoveProductCommand = new RelayCommand(RemoveProduct);
             EditProductCommand = new RelayCommand(EditProduct);
 
             AddNewSpiceCommand = new RelayCommand(AddNewSpice);
+            AddSpiceToBaseCommand = new RelayCommand(AddSpiceToBase);
             RemoveSpiceCommand = new RelayCommand(RemoveSpice);
             EditSpiceCommand = new RelayCommand(EditSpice);
+            EditSpiceInBaseCommand = new RelayCommand(EditSpiceInBase);
 
             AddSpiceToWarehouseCommand = new RelayCommand(AddSpiceToWarehouse);
+            AddSpiceToWarehouseInBaseCommand = new RelayCommand(AddSpiceToWarehouseInBase);
             RemoveSpiceFromWarehouseCommand = new RelayCommand(RemoveSpiceFromWarehouse);
             EditSpiceInWarehouseCommand = new RelayCommand(EditSpiceInWarehouse);
 
             StartProductionCommand = new RelayCommand(StartProduction);
             ExportToExcelCommand = new RelayCommand(ExportToExcel);
             SaveCommand = new RelayCommand(Save);
+            CloseWindowCommand = new RelayCommand(CloseWindow);
+            ShowInfoCommand = new RelayCommand(ShowInfo);
+
             Test();
         }
 
@@ -171,17 +189,91 @@ namespace SpiceManager
 
         private void EditSpice(object obj)
         {
-            throw new NotImplementedException();
+            if (!ValidateParams(obj))
+            {
+                SetErrorMessage(ValidationMessages.NieWybranoElementuDoEdycji);
+            }
+            else
+            {
+                EditSpiceWindow editSpiceWindow = new EditSpiceWindow();
+                editSpiceWindow.DataContext = this;
+                editSpiceWindow.Owner = mainWindow;
+                editSpiceWindow.ShowDialog();
+            }
+        }
+
+        private void EditSpiceInBase(object obj)
+        {
+            var values = (object[])obj;
+            string newSpiceName = values[1].ToString();
+            bool isSpiceNameUnique = (!Spices.Any(x => x.Name.ToLower() == newSpiceName.ToLower()));
+            if (string.IsNullOrEmpty(newSpiceName))
+            {
+                SetErrorMessage(ValidationMessages.NiePodanoNazwy);
+            }
+            else if (!isSpiceNameUnique)
+            {
+                SetErrorMessage(ValidationMessages.NazwaPrzyprawyNieJestUnikalna);
+            }
+            else
+            {
+                EditSpiceWindow currentWindow = App.Current.Windows.OfType<EditSpiceWindow>().SingleOrDefault(x => x.IsActive);
+                Spice currentSpice = (Spice)values[0];
+                foreach (var item in Spices.Where(x => x.Id == currentSpice.Id))
+                {
+                    item.Name = newSpiceName;
+                }
+                UpdateProductAndWarehouseGrid(currentSpice.Id, newSpiceName);
+                SetErrorMessage(string.Empty);
+                currentWindow.Close();
+            }
         }
 
         private void RemoveSpice(object obj)
         {
-            throw new NotImplementedException();
+            if (!ValidateParams(obj))
+            {
+                SetErrorMessage(ValidationMessages.NieWybranoElementuDoUsuniecia);
+            }
+            var values = (object[])obj;
+            var spice = values[0];
+            Spices.Remove((Spice)spice);
         }
 
         private void AddNewSpice(object obj)
         {
-            throw new NotImplementedException();
+            SpiceWindow spiceWindow = new SpiceWindow();
+            spiceWindow.DataContext = this;
+            spiceWindow.Owner = mainWindow;
+            spiceWindow.ShowDialog();
+        }
+
+        private void AddSpiceToBase(object obj)
+        {
+            SpiceWindow currentWindow = App.Current.Windows.OfType<SpiceWindow>().SingleOrDefault(x=>x.IsActive);
+            string spiceName = currentWindow.SpiceName.Text.ToString();
+
+            bool isSpiceNameUnique = (!Spices.Any(x => x.Name.ToLower() == spiceName.ToLower()));
+
+            if (string.IsNullOrEmpty(spiceName))
+            {
+                SetErrorMessage(ValidationMessages.NiePodanoNazwy);
+            }
+            else if(!isSpiceNameUnique)
+            {
+                SetErrorMessage(ValidationMessages.NazwaPrzyprawyNieJestUnikalna);
+            }
+            else
+            {
+                Spice spice = new Spice
+                {
+                    Id = Helper.FindMaxValue(Spices, x => x.Id) + 1,
+                    Name = spiceName,
+                };
+                Spices.Add(spice);
+                SetErrorMessage(string.Empty);
+                currentWindow.Close();
+            }
         }
 
         #endregion
@@ -190,7 +282,38 @@ namespace SpiceManager
 
         private void AddNewProduct(object obj)
         {
+            AddProductWindow addProductWindow = new AddProductWindow();
+            addProductWindow.DataContext = this;
+            addProductWindow.Owner = mainWindow;
+            addProductWindow.ShowDialog();
+        }
 
+        public void AddProductToBase(object obj)
+        {
+            AddProductWindow currentWindow = App.Current.Windows.OfType<AddProductWindow>().SingleOrDefault(x => x.IsActive);
+            string productName = currentWindow.ProductName.Text.ToString();
+
+            bool isSpiceNameUnique = (!Products.Any(x => x.Name.ToLower() == productName.ToLower()));
+
+            if (string.IsNullOrEmpty(productName))
+            {
+                SetErrorMessage(ValidationMessages.NiePodanoNazwy);
+            }
+            else if (!isSpiceNameUnique)
+            {
+                SetErrorMessage(ValidationMessages.NazwaProduktuNieJestUnikalna);
+            }
+            else
+            {
+                Product product = new Product
+                {
+                    Id = Helper.FindMaxValue(Products, x => x.Id) + 1,
+                    Name = productName,
+                };
+                Products.Add(product);
+                SetErrorMessage(string.Empty);
+                currentWindow.Close();
+            }
         }
 
         private void EditProduct(object obj)
@@ -200,7 +323,13 @@ namespace SpiceManager
 
         private void RemoveProduct(object obj)
         {
-            throw new NotImplementedException();
+            if (!ValidateParams(obj))
+            {
+                SetErrorMessage(ValidationMessages.NieWybranoElementuDoUsuniecia);
+            }
+            var values = (object[])obj;
+            var product = values[0];
+            Products.Remove((Product)product);
         }
 
         #endregion
@@ -214,12 +343,54 @@ namespace SpiceManager
 
         private void RemoveSpiceFromWarehouse(object obj)
         {
-            throw new NotImplementedException();
+            if (!ValidateParams(obj))
+            {
+                SetErrorMessage(ValidationMessages.NieWybranoElementuDoUsuniecia);
+            }
+            var values = (object[])obj;
+            var spice = values[0];
+            Warehouse.Remove((Spice)spice);
         }
 
         private void AddSpiceToWarehouse(object obj)
         {
-            throw new NotImplementedException();
+            AddWarehouseSpiceWindow addWarehouseSpiceWindow = new AddWarehouseSpiceWindow();
+            addWarehouseSpiceWindow.DataContext = this;
+            addWarehouseSpiceWindow.Owner = mainWindow;
+            addWarehouseSpiceWindow.ShowDialog();
+        }
+
+        private void AddSpiceToWarehouseInBase(object obj)
+        {
+
+            AddWarehouseSpiceWindow currentWindow = App.Current.Windows.OfType<AddWarehouseSpiceWindow>().SingleOrDefault(x => x.IsActive);
+            string spiceName = currentWindow.SpiceWarehouseName.Text.ToString();
+            double spiceAmount = Double.Parse(currentWindow.SpiceWarehouseAmount.Text.ToString());
+            string spicePart = currentWindow.SpiceWarehousePart.Text.ToString();
+
+            bool isSpiceNameUnique = (!Warehouse.Any(x => x.Name.ToLower() == spiceName.ToLower()));
+
+            if (string.IsNullOrEmpty(spiceName))
+            {
+                SetErrorMessage(ValidationMessages.NiePodanoNazwy);
+            }
+            else if (!isSpiceNameUnique)
+            {
+                SetErrorMessage(ValidationMessages.NazwaPrzyprawyNieJestUnikalna);
+            }
+            else
+            {
+                Spice spice = new Spice
+                {
+                    Id = Helper.FindMaxValue(Spices, x => x.Id) + 1,
+                    Name = spiceName,
+                    Amount=spiceAmount,
+                    Part=spicePart,
+                };
+                Warehouse.Add(spice);
+                SetErrorMessage(string.Empty);
+                currentWindow.Close();
+            }
         }
 
         #endregion
@@ -240,18 +411,82 @@ namespace SpiceManager
         {
             throw new NotImplementedException();
         }
+
+        private void CloseWindow(object obj)
+        {
+            var currentWindow=App.Current.Windows.OfType<Window>().SingleOrDefault(x => x.IsActive);
+            SetErrorMessage(String.Empty);
+            currentWindow.Close();
+        }
+
+        private void ShowInfo(object obj)
+        {
+
+        }
+
+        public void UpdateProductAndWarehouseGrid(int id, string newSpiceName)
+        {
+            foreach (var item in Warehouse.Where(x=>x.Id==id))
+            {
+                item.Name = newSpiceName;
+            }
+            foreach (var item in Products)
+            {
+                foreach (var spice in item.SpiceList.Where(x=>x.Id==id))
+                {
+                    spice.Name = newSpiceName;
+                }
+            }
+        }
+
+        public bool ValidateParams(object parameter)
+        {
+            if (parameter == null)
+            {
+                return false;
+            }
+            var values = (object[])parameter;
+            foreach (var item in values)
+            {
+                if (item == null)
+                {
+                    return false;
+                }
+                if (item as String == string.Empty)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public bool ValidateParamsAsObject(object parameter)
+        {
+            if (parameter == null)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private void SetErrorMessage(string message)
+        {
+            mainWindow.ErrorTexBlock.Text = message;
+        }
+
         #endregion
 
         #endregion
 
         private void Test()
         {
-            Spice s = new Spice { Id = 1, Name = "Oregano", Amount = 10, Part = "D2aF" };
+            Spice s = new Spice { Id = 1, Name = "Oregano"};
+            Spice s2 = new Spice { Id = 1, Name = "Oregano", Amount = 10, Part = "D2aF" };
             Product p = new Product { Id = 6, Name = "Krakowska", SpiceList = new List<Spice> {s} };
             History.Add(new HistoryRecord { Id = 9, Date = DateTime.Now, SpiceList = new List<Spice> { s}, Text = "sabfds" });
             Spices.Add(s);
             Products.Add(p);
-            Warehouse.Add(s);
+            Warehouse.Add(s2);
         }
     }
 }
